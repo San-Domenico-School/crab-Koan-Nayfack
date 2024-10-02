11405c8b327b66c0f5df77586961e6189d44c385
Koan-Nayfack <knayfack25@sandomenico.org>
2024-10-02 12:37:57 -0700

    Update Worm.java

--- Worm.java
+++ Worm.java
@@ -3,8 +3,8 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class Worm here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Koan Nayfack) 
+ * @version (1.0)
  */
 public class Worm extends Actor
 {
